ef58c134b2fdf0e56e883143301aaba98359b256
saggins <win32prog@gmail.com>
2021-06-04 15:15:35 -0700

    First Commit

---
+++ shell.nix
@@ -0,0 +1,16 @@
+let
+  # Niv
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+  inherit (pkgs.lib) optional optionals;
+  # Python
+  python-env = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./.;
+  };
+in pkgs.mkShell {
+  buildInputs = [
+    python-env
+    pkgs.poetry
+  ];
+
+}
