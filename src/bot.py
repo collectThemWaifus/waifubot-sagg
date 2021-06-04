6e7cb83b3df93ddcc39bb530abc64d524d8748f8
Neil <qh4nveh@gmail.com>
2021-06-04 16:28:55 -0700

    removed token

--- src/bot.py
+++ src/bot.py
@@ -32,7 +32,7 @@ async def waifu(ctx):
     embed.set_image(url='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/053b52c1-e89f-469b-aacb-972f74758f24/ddluws0-67fa36cc-0007-4e27-8bdf-46aefed76605.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzA1M2I1MmMxLWU4OWYtNDY5Yi1hYWNiLTk3MmY3NDc1OGYyNFwvZGRsdXdzMC02N2ZhMzZjYy0wMDA3LTRlMjctOGJkZi00NmFlZmVkNzY2MDUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pzbBFaABSy5xE-2yDGXwGZiibBLr4jcK_oh_dJ6Z-sk ')
     await ctx.send(embed=embed)
 
-client.run('ODQyODMyODc4NTU0MzE2ODM3.YJ7DSQ.fTV7GHAz9ynXh9bN4v5AfRgoQBM')
+client.run(token)
 
 #TODO: Create a caste system for waifu / husbandos
 # Use ANILIST for caste
