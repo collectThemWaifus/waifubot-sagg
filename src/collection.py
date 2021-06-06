edb847c17224d5444f2a7200a7df466ba5cce35c
Brian <shih.brian123@gmail.com>
2021-06-05 20:52:45 -0700

    added find number of waifus

--- src/collection.py
+++ src/collection.py
@@ -1,24 +1,36 @@
 import requests
-# Here we define our query as a multi-line string
-query = '''
-query ($id: Int) { # Define which variables will be used in the query (id)
-  Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-    id
-    title {
-      romaji
-      english
-      native
-    }
-  }
+import json
+
+def findWaifu(perpage):
+  # Here we define our query as a multi-line string
+  query = '''
+  query ($perpage: Int) { # Define which variables will be used in the query (id) 
+    Page(page: 50, perPage: $perpage) { 
+       characters(sort: FAVOURITES_DESC){
+          image{
+            medium
+          }
+          favourites
+          name {
+            full
+            
+          }
+        }
+      }
+    } 
+
+  '''
+
+  # Define our query variables and values that will be used in the query request
+  variables = {
+      'perpage': perpage
 }
-'''
 
-# Define our query variables and values that will be used in the query request
-variables = {
-    'id': 15125
-}
+  url = 'https://graphql.anilist.co'
 
-url = 'https://graphql.anilist.co'
+  # Make the HTTP Api request
+  response = requests.post(url, json={'query': query, 'variables': variables})
+  return(response.text)
 
-# Make the HTTP Api request
-response = requests.post(url, json={'query': query, 'variables': variables})
+x = json.loads(findWaifu(30))
+print(x)
