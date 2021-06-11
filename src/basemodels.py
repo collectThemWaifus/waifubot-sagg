47ef7ba8a50a102e28f71f70c4356ed0ee0b7bb3
saggins <win32prog@gmail.com>
2021-06-11 15:03:20 -0700

    removed default values

--- src/basemodels.py
+++ src/basemodels.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel
 
 class Waifu(BaseModel):
     favourites: int
-    imageURL: str ='https://images-na.ssl-images-amazon.com/images/I/61iwElTo6ML.jpg'
-    name: str = ''
+    imageURL: str 
+    name: str 
