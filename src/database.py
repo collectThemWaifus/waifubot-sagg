308fb390e74274dad05a2cd80738932c9f46bb78
TimeGlitch <frankj.zhang@gmail.com>
2021-06-13 21:20:15 -0700

    added sacrificed row

--- src/database.py
+++ src/database.py
@@ -12,6 +12,7 @@ def databaseSetup():
             imageURL VARCHAR(100) NOT NULL,
             favourites MEDIUMINT NOT NULL,
             time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            sacrificed INT NOT NULL
             PRIMARY KEY (userid, time)
         );
     '''
@@ -20,8 +21,8 @@ def databaseSetup():
     con.commit()
     print("Tables Ready!")
 def storeWaifu(waifu : Waifu, userid : str):
-    sql_storeWaifu = "INSERT INTO userWaifu (userid, name, imageURL , favourites) VALUES (?, ?, ?, ?)"
-    sql_vals = (userid, waifu.name , waifu.imageURL, waifu.favourites )
+    sql_storeWaifu = "INSERT INTO userWaifu (userid, name, imageURL , favourites, sacrificed) VALUES (?, ?, ?, ?, ?)"
+    sql_vals = (userid, waifu.name , waifu.imageURL, waifu.favourites, 0 )
     con = sqlite3.connect('waifuUser.db')
     con.execute(sql_storeWaifu, sql_vals)
     con.commit()
