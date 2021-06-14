80b925864398baa3e11ae29234600d51fdceeaae
Neil <qh4nveh@gmail.com>
2021-06-13 20:45:08 -0700

    Updated help function

--- src/bot.py
+++ src/bot.py
@@ -15,6 +15,13 @@ intents = discord.Intents.all()
 client = commands.Bot(command_prefix = '-', intents=intents)
 client.remove_command('help')
 
+def getnum(str):
+    ret = ''
+    for i in range(len(str)):
+        if str[i].isdigit():
+            ret += str[i]
+    return(ret)
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -57,6 +64,8 @@ async def on_reaction_add(reaction, user):
         embed2.add_field(name = 'Name', value = f'{messageWaifu.name}', inline = False)
         await reaction.message.edit(embed=embed2)  
         storeWaifu(messageWaifu, user.id)
+
+
 @client.command(aliases = ['wa'])
 async def waifu(ctx : commands.Context):
     valid_reactions = ['👍']
@@ -80,11 +89,16 @@ async def waifu(ctx : commands.Context):
 @client.command(aliases = ['h'])
 async def help(ctx):
     embed = discord.Embed(
-        title = 'Help menu for Waifu Bot', 
-        description = 'Commands:',
+        title = 'Help',
+        description = 'List of commands for Waifu Bot',
         colour = discord.Colour.green()
     )
-    embed.set_thumbnail(url = 'https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg')
+    #embed.set_thumbnail(url = 'https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg')
+    embed.add_field(name = 'waifu (wa)', value = 'Displays a random waifu, react to claim.', inline = False)
+    embed.add_field(name = 'inventory (inv)', value = 'Sends you a link to a website containing your inventory', inline = False)
+    embed.add_field(name = 'help (h)', value = 'Displays this message!', inline = False)
+    embed.add_field(name = 'setlb [channel]', value = 'Set a specific channel to display the leaderboard', inline = False)
+    embed.set_footer(text="This bot is still in development!")
     await ctx.send(embed=embed)
 
 @client.command(aliases = ['inv'])
@@ -97,6 +111,20 @@ async def inventory(ctx):
     )
     await ctx.message.author.send(embed=embed)
 
+
+@client.command()
+@commands.has_permissions(manage_guild = True)
+async def setlb(ctx, channel):
+    await ctx.send(f'Set the leaderboard to {channel}')
+    channel = getnum(str(channel))
+    channel = int(channel)
+    channel = client.get_channel(channel)
+    await channel.send("test")
+
+@setlb.error
+async def setlb_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("**You dont have permission to do that!**")
 client.run(token)
 
 #TODO: Create a caste system for waifu / husbandos
