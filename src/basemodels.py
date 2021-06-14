2aecbd6ef0901b36f7b111c188408fa32ee0ac0f
TimeGlitch <frankj.zhang@gmail.com>
2021-06-13 21:22:26 -0700

    add sac

--- src/basemodels.py
+++ src/basemodels.py
@@ -4,6 +4,7 @@ class Waifu(BaseModel):
     favourites: int
     imageURL: str 
     name: str 
+    sacrificed: int # 0 for alive, 1 for has been sacrificed
 
 class User(BaseModel):
     name: str
