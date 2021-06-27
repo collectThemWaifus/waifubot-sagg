4ec3bd4cb02252ee7ccf5d2f4e8d15fb1b8dd9d0
saggins <win32prog@gmail.com>
2021-06-27 15:24:11 -0700

    Significantly improved speed of castewaifu

--- src/collection.py
+++ src/collection.py
@@ -44,37 +44,37 @@ def GetCasteWaifu(ranking): #page 67, threshold = 500 fav min
   if ranking == "SS":  #20-60th - 3% (SS) 12k - 8.2k favourites
     waifulist = findWaifu(20,2)
     waifulist = waifulist + findWaifu(20,3)
-    return(waifulist[random.randrange(40)])
+    return(waifulist[random.randrange(20)])
 
   if ranking == "S":  #60-120th - 8% (S) 8k - 5.4k favourites
     waifulist = []
-    for x in range(4,7):
-      waifulist = waifulist + findWaifu(20,x)
-    return(waifulist[random.randrange(60)])
+    x = random.randrange(4,7)
+    waifulist = waifulist + findWaifu(20,x)
+    return(waifulist[random.randrange(20)])
   
   if ranking == "A": #120-200th - 14% (A) 5.4k - 3.7k favourites 
     waifulist = []
-    for x in range(7,11):
-      waifulist = waifulist + findWaifu(20,x)
-    return(waifulist[random.randrange(80)])
+    x = random.randrange(7,11)
+    waifulist = waifulist + findWaifu(20,x)
+    return(waifulist[random.randrange(20)])
 
   if ranking == "B": #200-420th - 22% (B) 3.7k - 2k favourites
     waifulist = []
-    for x in range(11,22):
-      waifulist = waifulist + findWaifu(20,x)
-    return(waifulist[random.randrange(220)])
+    x = random.randrange(11,22)
+    waifulist = waifulist + findWaifu(20,x)
+    return(waifulist[random.randrange(20)])
 
   if ranking == "C": #420-820th - 31% (C) 2k - 934 favourites
     waifulist = []
-    for x in range(22,42):
-      waifulist = waifulist + findWaifu(20,x)
-    return(waifulist[random.randrange(400)])
+    x = random.randrange(22,42)
+    waifulist = waifulist + findWaifu(20,x)
+    return(waifulist[random.randrange(20)])
 
   if ranking == "D": #820-1320th - 37% (D) 934 - 525 favourites
-    waifulist = [] 
-    for x in range(42,67):
-      waifulist = waifulist + findWaifu(20,x)
-    return(waifulist[random.randrange(500)])
+    waifulist = []
+    x = random.randrange(42, 52)
+    waifulist += findWaifu(20,x)
+    return(waifulist[random.randrange(20)])
 
     
 
