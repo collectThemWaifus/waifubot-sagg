df5abf2eb87a10878b5693cf8ab1764a16f5bab2
saggins <win32prog@gmail.com>
2021-07-13 23:23:25 -0700

    commented temp code

--- src/discord/cogs/leaderboard.py
+++ src/discord/cogs/leaderboard.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
 from discord import client
-from discord import Embed
-from discord import Colour
+# from discord import Embed
+# from discord import Colour
 
-class leaderboard( commands.Cog ):
+
+class leaderboard(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
@@ -14,24 +15,21 @@ class leaderboard( commands.Cog ):
                 ret += str[i]
         return(ret)
 
-    
     @commands.command()
-    @commands.has_permissions(manage_guild = True)
+    @commands.has_permissions(manage_guild=True)
     async def setlb(self, ctx, channel):
         await ctx.send(f'Set the leaderboard to {channel}')
         channel = self.getnum(str(channel))
         channel = int(channel)
         channel = client.get_channel(channel)
-        embed = Embed(
-            title = 'Help',
-            description = 'List of commands for Waifu Bot',
-            colour = Colour.blue()
-        )
+        # embed = Embed(
+        #     title='Help',
+        #     description='List of commands for Waifu Bot',
+        #     colour=Colour.blue()
+        # )
         await channel.send("test")
 
     @setlb.error
     async def setlb_error(ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("**You dont have permission to do that!**")
-
-
